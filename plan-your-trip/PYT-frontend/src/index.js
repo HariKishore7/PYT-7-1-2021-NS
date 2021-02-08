924e35afdcf92d50bee8238fb1f17e451ffc4ccd
HariKishore7 <harikishoret99@gmail.com>
2021-02-08 20:05:05 +0530

    All files are up-to date 8-2-21

--- plan-your-trip/PYT-frontend/src/index.js
+++ plan-your-trip/PYT-frontend/src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import HomePage from "./HomePage/HomePage";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-// ReactDOM.render(<Welcome/>,document.getElementById("root"));
